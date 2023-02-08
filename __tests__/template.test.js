bd75e08f29c3c3862b06464c2467818a49162f22
Mike Donovan <mike@mikedonovan.dev>
2023-02-08 08:48:52 -0800

    Initial commit

---
+++ __tests__/template.test.js
@@ -0,0 +1,14 @@
+// import Object from './../src/TEMPLATE.js';
+// test 1('', () => {
+//   const  = new 
+//   expect(x).toEqual(y);
+//   expect(triangle.side2).toEqual(4);
+//   expect(triangle.side3).toEqual(5);
+// });
+
+// test 2('', () => {
+//   const  = new 
+//   expect(x).toEqual(y);
+//   expect(triangle.side2).toEqual(4);
+//   expect(triangle.side3).toEqual(5);
+// });
